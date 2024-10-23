50898b0b0af725702a666d5637153d3ef7adec5d
Unho Chang <unho.chang@gmail.com>
2024-10-23 18:19:58 +0900

    작업 마무리 커밋

--- 전출_시각화결과_pdf_title_image_생성기02.py
+++ 전출_시각화결과_pdf_title_image_생성기02.py
@@ -24,8 +24,8 @@ pdfmetrics.registerFont(TTFont("맑은고딕", "Malgun.ttf"))
 pdfmetrics.registerFont(TTFont("맑은고딕체", "Malgunbd.ttf"))
 
 
-dir_nm = "c:/projects/kosis_dn/광역_전입사유_plot01/"
-title_txt = "광역 전입사유별 순위변동 추이"
+dir_nm = "c:/projects/kosis_dn/시군구_전입_전출_사유_plot/"
+title_txt = "시군구_전입전출_사유별_순위변동_추이"
 
 # 1) 프레임 생성
 landscape(A4)[0], landscape(A4)[1]
@@ -100,58 +100,9 @@ class NumberedPageTemplate(PageTemplate):
         canvas.drawString(page_width - 300, 30, page_number_text)
         canvas.restoreState()
 
-doc = BaseDocTemplate(str(dir_nm + title_txt + '.pdf'), pagesize=landscape(A4))
+doc = BaseDocTemplate(str("c:/projects/kosis_dn/plot_pdf_aggr/" + title_txt +"_" +datetime.now().strftime("%Y-%m-%d")+'.pdf'), pagesize=landscape(A4))
 image_page = NumberedPageTemplate(id='SinglePage',
                      frames=[text_frame]    # 4. 프레임 지정 부분에서 text_frame 생성했음
             )
 doc.addPageTemplates(image_page)
 doc.build(L)
-
-
-# 3) PDF 생성
-# L = []
-# for num, file in enumerate(png_list):
-#     print(num, file)
-#     im = Image.open(dir_nm + file)
-#     nm = file.split(".")[0]
-#     imgdata = BytesIO(im.tobytes())
-#     im.save(imgdata, 'PNG')
-#     imgdata.seek(0)
-#     L.append(platypus.Image(imgdata, width=24.16 * cm, height=15.92 * cm))
-
-# class NumberedPageTemplate(PageTemplate):
-#     def beforeDrawPage(self, canvas, doc):
-#         canvas.saveState()
-#         canvas.setFont('맑은고딕', 10)
-#         page_number_text = f"Page {doc.page}"
-#         canvas.drawString(page_width - 100, 30, page_number_text)
-#         canvas.restoreState()
-
-# class NumberedPageTemplate2(PageTemplate):
-#     def beforeDrawPage(self, canvas, doc):
-#         canvas.saveState()
-#         canvas.setFont('맑은고딕', 10)
-#         page_number_text = f"Page {doc.page}"
-#         canvas.drawString(page_width - 100, 30, page_number_text)
-        
-#         # 첫 번째 페이지에 제목과 날짜 추가
-#         if doc.page == 1:
-#             print("Adding title and date to the first page")  # 디버그용 출력
-#             title = title_txt
-#             date_str = datetime.now().strftime("%Y-%m-%d")
-#             canvas.setFont('맑은고딕체', 20)
-#             canvas.drawString(page_width / 2, page_height - 50, title)
-#             canvas.setFont('맑은고딕', 12)
-#             canvas.drawString(page_width / 2, page_height - 80, date_str)
-        
-#         canvas.restoreState()
-
-# doc = BaseDocTemplate(str(dir_nm + title_txt + '.pdf'), pagesize=landscape(A4))
-# image_page = NumberedPageTemplate2(id='SingleFrame',
-#                      frames=[text_frame]    # 4. 프레임 지정 부분에서 text_frame 생성했음
-#             )
-# doc.addPageTemplates(image_page)
-# doc.build(L)
-
-
-
